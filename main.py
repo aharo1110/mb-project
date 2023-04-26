36597cb66bd69c31cacfc2a7db6675b7785f9aa2
aharo1110 <aharo1110@gmail.com>
2023-04-26 15:01:19 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 import requests, json
+from flask import Flask, render_template, redirect
+from flask_bootstrap import Bootstrap5
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField
+from wtforms.validators import DataRequired
+
+app = Flask(__name__)
+bootstrap = Bootstrap5(app)
+
+import os
+SECRET_KEY = os.urandom(32)
+app.config['SECRET_KEY'] = SECRET_KEY
