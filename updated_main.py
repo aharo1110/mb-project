80745a70b36dff997db878af222528dec798d115
Conor Makowski <112346125+ConorMako@users.noreply.github.com>
2023-05-13 21:48:27 -0700

    Now allows the user to search for a date

--- updated_main.py
+++ updated_main.py
@@ -1,9 +1,6 @@
-import requests, json
-from flask import Flask, render_template, redirect
+import requests
+from flask import Flask, render_template, redirect, request
 from flask_bootstrap import Bootstrap5
-from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField
-from wtforms.validators import DataRequired
 from datetime import datetime
 
 app = Flask(__name__)
@@ -16,17 +13,30 @@ app.config['SECRET_KEY'] = SECRET_KEY
 today = datetime.now()
 thism = today.month
 thisd = today.day
-endpoint = endpoint = f"https://en.wikipedia.org/api/rest_v1/feed/onthisday/events/{thism}/{thisd}"
-
+endpoint = f"https://en.wikipedia.org/api/rest_v1/feed/onthisday/events/{thism}/{thisd}"
 
 @app.route('/', methods=('GET', 'POST'))
 def main():
-    m = 13
-    setdate = datetime(2023, thism, thisd)
-    printdate = f"{setdate.strftime('%B')} {thisd}"
-    try:
-        r = requests.get(endpoint)
-        data = r.json()
-    except:
-        print('please try again')
-    return render_template('index.html', data=data, m=m, d=printdate)
+    global thism, thisd, endpoint
+
+    if request.method == 'POST':
+        selected_date = request.form['date-pick']
+        try:
+            selected_date = datetime.strptime(selected_date, '%Y-%m-%d')
+            thism = selected_date.month
+            thisd = selected_date.day
+            endpoint = f"https://en.wikipedia.org/api/rest_v1/feed/onthisday/events/{thism}/{thisd}"
+            r = requests.get(endpoint)
+            data = r.json()
+            return render_template('index.html', data=data, m=0, d=selected_date.strftime('%B %d'))
+        except ValueError:
+            return redirect('/')
+    else:
+        setdate = datetime(2023, thism, thisd)
+        printdate = f"{setdate.strftime('%B')} {thisd}"
+        try:
+            r = requests.get(endpoint)
+            data = r.json()
+        except:
+            print('please try again')
+        return render_template('index.html', data=data, m=13, d=printdate)
