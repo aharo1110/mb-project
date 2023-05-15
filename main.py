bc0ab772da2d2224ebcb154c4a66325ae6ffb520
aharo1110 <aharo1110@gmail.com>
2023-05-15 16:54:30 -0700

    added item switcher, cleaned up formatting

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import requests, json
-from flask import Flask, render_template, redirect
+import requests
+from flask import Flask, render_template, redirect, request
 from flask_bootstrap import Bootstrap5
-from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField
-from wtforms.validators import DataRequired
-from datetime import date
+from datetime import datetime
 
 app = Flask(__name__)
 bootstrap = Bootstrap5(app)
@@ -13,19 +10,61 @@ import os
 SECRET_KEY = os.urandom(32)
 app.config['SECRET_KEY'] = SECRET_KEY
 
-thism = 5
-thisd = 4
-endpoint = endpoint = f"https://en.wikipedia.org/api/rest_v1/feed/onthisday/events/{thism}/{thisd}"
+today = datetime.now()
+thism = today.month
+thisd = today.day
+endpoint = f"https://en.wikipedia.org/api/rest_v1/feed/onthisday/events/{thism}/{thisd}"
+item = 0
+l = ""
+z = 0
 
 
 @app.route('/', methods=('GET', 'POST'))
 def main():
-    m = 13
-    setdate = date(2023, thism, thisd)
-    printdate = f"{setdate.strftime('%B')} {thisd}"
-    try:
-        r = requests.get(endpoint)
-        data = r.json()
-    except:
-        print('please try again')
-    return render_template('index.html', data=data, m=m, d=printdate)
+    global thism, thisd, endpoint, item, l, z
+
+    if request.method == 'POST' and 'date-pick' in request.form:
+        selected_date = request.form['date-pick']
+        try:
+            selected_date = datetime.strptime(selected_date, '%Y-%m-%d')
+            thism = selected_date.month
+            thisd = selected_date.day
+            l = f"{selected_date.strftime('%B')} {thisd}"
+            endpoint = f"https://en.wikipedia.org/api/rest_v1/feed/onthisday/events/{thism}/{thisd}"
+            r = requests.get(endpoint)
+            data = r.json()
+            item = 0
+            z = len(data["events"])
+            return render_template('index.html', data=data, m=item, d=f"{selected_date.strftime('%B')} {thisd}")
+        except ValueError:
+            return redirect('/')
+    elif request.method == 'POST' and 'shift' in request.form:
+        try:
+            r = requests.get(endpoint)
+            data = r.json()
+        except:
+            print('please try again')
+
+        if request.form['shift'] == 'back':
+            if item > 0:
+                item = item - 1
+                return render_template('index.html', data=data, m=item, d=l)
+            else:
+                return redirect("/")
+        elif request.form['shift'] == 'next':
+            if item < z:
+                item = item + 1
+                return render_template('index.html', data=data, m=item, d=l)
+            else:
+                return redirect("/")
+    else:
+        setdate = datetime(2023, thism, thisd)
+        printdate = f"{setdate.strftime('%B')} {thisd}"
+        l = printdate
+        try:
+            r = requests.get(endpoint)
+            data = r.json()
+            z = len(data["events"])
+        except:
+            print('please try again')
+        return render_template('index.html', data=data, m=item, d=l)
---
+++ old_main.py
@@ -0,0 +1,31 @@
+import requests, json
+from flask import Flask, render_template, redirect
+from flask_bootstrap import Bootstrap5
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField
+from wtforms.validators import DataRequired
+from datetime import date
+
+app = Flask(__name__)
+bootstrap = Bootstrap5(app)
+
+import os
+SECRET_KEY = os.urandom(32)
+app.config['SECRET_KEY'] = SECRET_KEY
+
+thism = 5
+thisd = 4
+endpoint = endpoint = f"https://en.wikipedia.org/api/rest_v1/feed/onthisday/events/{thism}/{thisd}"
+
+
+@app.route('/', methods=('GET', 'POST'))
+def main():
+    m = 13
+    setdate = date(2023, thism, thisd)
+    printdate = f"{setdate.strftime('%B')} {thisd}"
+    try:
+        r = requests.get(endpoint)
+        data = r.json()
+    except:
+        print('please try again')
+    return render_template('index.html', data=data, m=m, d=printdate)
