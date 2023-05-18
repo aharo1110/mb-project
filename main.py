6c0d5d86d8f99a62c3e8e56dd74cf28e5ef83cb4
RajveerSaini01 <rajveersaini02@gmail.com>
2023-05-18 13:29:03 -0700

    Added Comments
    Added comments to main.py file

--- main.py
+++ main.py
@@ -9,10 +9,11 @@ bootstrap = Bootstrap5(app)
 import os
 SECRET_KEY = os.urandom(32)
 app.config['SECRET_KEY'] = SECRET_KEY
-
+#setting the user imput as a varible.
 today = datetime.now()
 thism = today.month
 thisd = today.day
+#Website API with user inputs
 endpoint = f"https://en.wikipedia.org/api/rest_v1/feed/onthisday/events/{thism}/{thisd}"
 item = 0
 l = ""
@@ -22,21 +23,27 @@ z = 0
 @app.route('/', methods=('GET', 'POST'))
 def main():
     global thism, thisd, endpoint, item, l, z
-
+    #when correct format of date is entered the program contiunes to the try statement  and executes.
     if request.method == 'POST' and 'date-pick' in request.form:
         selected_date = request.form['date-pick']
         try:
+            #Date entered is saved as selected_Date
             selected_date = datetime.strptime(selected_date, '%Y-%m-%d')
+            #today.month, user selected month
             thism = selected_date.month
+              #today.day, user selected day
             thisd = selected_date.day
             l = f"{selected_date.strftime('%B')} {thisd}"
+            #Adds date into API
             endpoint = f"https://en.wikipedia.org/api/rest_v1/feed/onthisday/events/{thism}/{thisd}"
             r = requests.get(endpoint)
             data = r.json()
             item = 0
+            #Length of api within seached field tp display correct total value.
             z = len(data["events"])
             return render_template('index.html', data=data, m=item, d=f"{selected_date.strftime('%B')} {thisd}")
         except ValueError:
+            #if not vaild date the program will default back to / directory
             return redirect('/')
     elif request.method == 'POST' and 'shift' in request.form:
         try:
@@ -46,16 +53,20 @@ def main():
             print('please try again')
 
         if request.form['shift'] == 'back':
+            # when back  arrow is pressed the item value varible is subtracted by 1 and then the program is ran again
             if item > 0:
                 item = item - 1
                 return render_template('index.html', data=data, m=item, d=l)
             else:
+                #Reroute to home directory
                 return redirect("/")
         elif request.form['shift'] == 'next':
+            # when forward  arrow is pressed the item value varible is subtracted by 1 and then the program is ran again
             if item < z:
                 item = item + 1
                 return render_template('index.html', data=data, m=item, d=l)
             else:
+                      #Reroute to home directory
                 return redirect("/")
     else:
         setdate = datetime(2023, thism, thisd)
